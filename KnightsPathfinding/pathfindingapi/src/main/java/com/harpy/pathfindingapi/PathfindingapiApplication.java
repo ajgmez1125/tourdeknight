78cc703dcc147d1d63a3f1512facdd6f1764de24
Ayden Gomez <ajgg1125@gmail.com>
2023-09-21 17:10:17 -0400

    worked on some of the deployment details regarding the api

--- KnightsPathfinding/pathfindingapi/src/main/java/com/harpy/pathfindingapi/PathfindingapiApplication.java
+++ KnightsPathfinding/pathfindingapi/src/main/java/com/harpy/pathfindingapi/PathfindingapiApplication.java
@@ -2,11 +2,19 @@ package com.harpy.pathfindingapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;  
 
 @SpringBootApplication
-public class PathfindingapiApplication {
+public class PathfindingapiApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(PathfindingapiApplication.class, args);
 	}
+
+	@Override  
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application)   
+	{  
+		return application.sources(PathfindingapiApplication.class);  
+	}  
 }
